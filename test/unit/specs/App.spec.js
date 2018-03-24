7eeb3d8b9df6b646f0d82e2977f30f7980b72c4a
Gervel Giva <gervelgiva@gmail.com>
2018-03-24 23:55:35 +0800

    Completed migration to vue-test-utils

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,34 +1,17 @@
 import { shallow } from '@vue/test-utils';
 import Vue from 'vue';
 import App from '@/App';
-// import router from '@/router/index';
 import Header from '@/components/common/Header';
-
-const EXPECTED_COMPONENTS = ['router-view', 'router-link', Header.tag];
 
 describe('App.vue', () => {
   it('should have a name', () => {
     expect(App.name).to.equal('App');
   });
 
-  xit('should have the necessary components', () => {
-    const Constructor = Vue.extend(App);
-    const vm = new Constructor({ router }).$mount();
-
-    for (let i in EXPECTED_COMPONENTS) {
-      expect(vm.$options.components).to.have.property(EXPECTED_COMPONENTS[i]);
-    }
-  });
-
   it('should have the necessary components', () => {
-    const wrapper = shallow(App, {
-      components: { [Header.tag]: Header }
-    });
-    // const vm = wrapper.vm;
-    // for(let i in wrapper) {
-    //   console.log(i);
-    // }
+    const wrapper = shallow(App);
+
+    expect(wrapper.contains(Header)).to.true;
   });
 
-  // should contain rendered "header"
 });
